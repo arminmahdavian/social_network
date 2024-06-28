82215a72393bc96e834f1907dbbbb29666f3af22
Armin <armin.mail007@gmail.com>
2024-06-28 22:35:33 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    abbr = models.CharField(max_length=5)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+        # db_table = 'countries'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.BigIntegerField(blank=True, null=True, unique=True)
+    country = models.ForeignKey(to=Country, on_delete=models.CASCADE, related_name='countries')
+    avatar = models.ImageField(blank=True, null=True)
+
+
+class Device(models.Model):
+    DEVICE_WEB = 1
+    DEVICE_IOS = 2
+    DEVICE_ANDROID = 3
+    DEVICE_PC = 4
+    DEVICE_TYPE_CHOICES = (
+        (DEVICE_WEB, 'web'),
+        (DEVICE_IOS, 'ios'),
+        (DEVICE_ANDROID, 'android'),
+        (DEVICE_PC, 'pc'),
+    )
+
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='devices')
+    device_uuid = models.UUIDField('device UUID', null=True)
+    last_login = models.DateTimeField('last login date', null=True)
+    device_type = models.PositiveSmallIntegerField(choices=DEVICE_TYPE_CHOICES, default=DEVICE_WEB)
+    device_os = models.CharField('device os', max_length=20, blank=True, null=True)
+    device_model = models.CharField('device model', max_length=50, blank=True, null=True)
+    app_version = models.CharField('app version', max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
